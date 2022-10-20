340a926e5d6d3e98f708d75b72f6908b813084d3
Samuel Zurowski <szuro1@unh.newhaven.edu>
2022-10-19 23:51:52 -0400

    Setup basic button clicks for fragments

--- app/src/main/java/com/toolkit/sit/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/toolkit/sit/fragments/login/LoginFragment.kt
@@ -1,10 +1,16 @@
 package com.toolkit.sit.fragments.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.toolkit.sit.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,47 +20,55 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [LoginFragment] factory method to
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // https://stackoverflow.com/questions/13216916/how-to-replace-the-activitys-fragment-from-the-fragment-itself
+    private var TAG = "LOGIN_FRAGMENT"
+//    private lateinit var auth: FirebaseAuth
+    private lateinit var loginButton: Button
+    private lateinit var forgotPasswordButton: Button
+    private lateinit var signUpButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var usernameField: EditText
+    private lateinit var passwordField: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        val view: View = inflater!!.inflate(R.layout.login_fragment, container, false)
+
+        loginButton = view.findViewById(R.id.buttonLogin)
+        forgotPasswordButton = view.findViewById(R.id.buttonForgotPassword)
+        signUpButton = view.findViewById(R.id.buttonSignUp)
+        usernameField = view.findViewById(R.id.usernameField)
+        passwordField = view.findViewById(R.id.passwordField)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        loginButton.setOnClickListener {
+            val user = usernameField.text
+            val password = passwordField.text
+            Log.d(TAG, "Login Data: ${user}:${password}")
+        }
+        forgotPasswordButton.setOnClickListener {
+            Log.d(TAG, "Forgot Button")
+        }
+        signUpButton.setOnClickListener {
+            Log.d(TAG, "Sign up button.")
+        }
+
+        super.onStart()
     }
+
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//
+//        super.onCreate(savedInstanceState)
+//    }
+
+
 }
